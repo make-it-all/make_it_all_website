0a8369ee4fa515b6692d3fe7fe9729580c7f912e
zachnurc <zach.nurcombe@gmail.com>
2017-02-13 20:56:41 +0000

    added basic controllers

---
+++ app/controllers/personnel.php
@@ -0,0 +1,18 @@
+<?php
+
+class PersonnelController extends Controller\Base {
+
+  public $view_folder = 'personnel';
+
+  public function index(){
+
+  }
+
+  public function edit(){
+
+  }
+
+  public function new(){
+
+  }
+}
---
+++ app/controllers/problems.php
@@ -0,0 +1,18 @@
+<?php
+
+class ProblemsController extends Controller\Base {
+
+  public $view_folder = 'problems';
+
+  public function index(){
+
+  }
+
+  public function edit(){
+
+  }
+
+  public function new(){
+
+  }
+}
