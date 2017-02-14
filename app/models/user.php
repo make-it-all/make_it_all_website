4fcd2220685816764a4f5641f9182e2b3fe3c141
Henry Morgan <henry@cd2solutions.co.uk>
2017-02-14 00:23:58 +0000

    added logs file and removed chronicle setup from users

--- app/models/user.php
+++ app/models/user.php
@@ -1,13 +1,5 @@
 <?php
 
-Chronicle\Base::setup_connection([
-  'driver'   => 'mysql',
-  'host'     => 'localhost',
-  'username' => 'root',
-  'password' => '',
-  'database' => 'make-it-all'
-]);
-
 class User extends Chronicle\Base {
 
   public static $table_name = 'users';
