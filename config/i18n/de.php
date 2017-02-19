ecb3b234931ed5b8c6c67e1b22fa254881f96519
zachnurc <zach.nurcombe@gmail.com>
2017-02-19 21:05:56 +0000

    added basic german translations

---
+++ config/i18n/de.php
@@ -0,0 +1,42 @@
+<?php
+
+$header = [
+  'signed_in_message'=>'Signed in as <b>Alice Help</b> - ',
+];
+
+$titles = [
+  'dashboard'=>'Dashboard',
+  'calls'=>'Telefonieren',
+  'problems'=>'Probleme',
+  'personnel'=>'Personal',
+  'users'=>'Users',
+  'reports'=>'Reports',
+  'settings'=>'Settings',
+  'hardware'=>'Hardware',
+  'software'=>'Software',
+  'title'=>'Make It All',
+  ];
+
+$actions = [
+  'new'=>'New',
+  'all'=>'All',
+  'edit'=>'Edit',
+  'delete'=>'Delete',
+  'search'=>'Search',
+];
+
+$roles = [
+  'admin'=>'Admin',
+  'operator'=>'Operator',
+  'specialist'=>'Specialist',
+  'lboroadmin'=>'Lboro Admin',
+];
+
+$table_headings = [
+  'id'=>'ID',
+  'name'=>'Name',
+  'email'=>'Email',
+  'last_seen_at'=>'Last Seen At',
+  'role'=>'Role',
+  'type'=>'Type',
+];
