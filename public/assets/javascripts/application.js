2337fc5e8ba0afa7201473651f1873c5ce35e7de
Henry Morgan <henry@cd2solutions.co.uk>
2017-02-19 21:10:04 +0000

    fixed data-method javascript

--- public/assets/javascripts/application.js
+++ public/assets/javascripts/application.js
@@ -5,6 +5,8 @@ $(function(){
     var href=this.href;
     var method = $(this).data('method');
     var $form = $('<form action="'+href+'" method="POST"><input type="hidden" name="__method" value="'+method+'"/></form>');
+    $form.css('display', 'none');
+    $('body').append($form);
     $form.submit()
   });
 
