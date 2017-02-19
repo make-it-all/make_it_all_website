5a866ba263caacbb02686247857c8a37f97d4294
zachnurc <zach.nurcombe@gmail.com>
2017-02-19 17:33:01 +0000

    added flags menu

--- app/views/application/_sidebar.php
+++ app/views/application/_sidebar.php
@@ -36,4 +36,13 @@
   <section>
     <?php echo $this->link_to('Settings'); ?>
   </section>
+  <div id="language_selector">
+    <input type="checkbox" class="dropdown_controller" id="language_dropdown"/>
+    <div class="dropdown_content">
+      <div class="dropdown_content"><a href="#"><?php echo $this->image_tag('arabic-flag'); ?>العربية</a></div>
+      <div class="dropdown_content"><a href="#"><?php echo $this->image_tag('chinese-flag'); ?>中文</a></div>
+      <div class="dropdown_content"><a href="#"><?php echo $this->image_tag('german-flag'); ?>Deutsch</a></div>
+    </div>
+    <label for="language_dropdown"><?php echo $this->image_tag('uk-flag'); ?>English</label>
+  </div>
 </div>
