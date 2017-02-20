96d1aa1adba71b45dccb16c120e116ad5a86624a
Henry Morgan <henry@cd2solutions.co.uk>
2017-02-20 05:33:19 +0000

    application_model

---
+++ app/models/application_model.php
@@ -0,0 +1,13 @@
+<?php
+
+class ApplicationModel extends \Chronicle\Base {
+
+  public function before_create() {
+    $this->created_at = new DateTime();
+  }
+
+  public function before_save() {
+    $this->updated_at = new DateTime();
+  }
+
+}
