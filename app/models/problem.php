0c41f790226e80ac1a6850539b60f59cf8108600
zachnurc <zach.nurcombe@gmail.com>
2017-02-20 00:49:26 +0000

    merged

--- app/models/problem.php
+++ app/models/problem.php
@@ -4,4 +4,8 @@ class Problem extends Chronicle\Base {
 
   public static $table_name = 'problems';
 
+  public function get_submitted_by() {
+    return User::find($this->get_attribute('submitted_by')->get());
+  }
+
 }
