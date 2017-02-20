3574cde533e4b4358ecb2dbb53746a4601571b2a
zachnurc <zach.nurcombe@gmail.com>
2017-02-20 01:33:53 +0000

    problems id's->names

--- app/models/problem.php
+++ app/models/problem.php
@@ -4,6 +4,14 @@ class Problem extends Chronicle\Base {
 
   public static $table_name = 'problems';
 
+  public function specialization() {
+    return Specializations::find($this->specialization_id);
+  }
+
+  public function get_assigned_to() {
+    return User::find($this->get_attribute('assigned_to')->get());
+  }
+
   public function get_submitted_by() {
     return User::find($this->get_attribute('submitted_by')->get());
   }
